f884926a628cfa11416b882904331cf73f6c14fc
littleblack111 <littleblack11111@gmail.com>
2024-04-30 12:40:17 +0800

    added stuff

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 if [ "$(uname)" == "Darwin" ]; then
   brew install libarchive
-else
-  pip install -r requirements.txt 
-  if [ ! $? -eq 0 ]; then
-    git clone https://github.com/seemoo-lab/opendrop.git
-    pip3 install ./opendrop
-  fi
+fi
+pip3 install -r requirements.txt 
+if [ ! $? -eq 0 ]; then
+  git clone https://github.com/seemoo-lab/opendrop.git
+  pip3 install ./opendrop
 fi
